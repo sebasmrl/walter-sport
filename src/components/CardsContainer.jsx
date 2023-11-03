63b2caf45526060ea2648f6238a2fc218a59cc32
Sebastian Morales <davesebastian99@gmail.com>
2023-11-03 11:42:33 -0500

    adeed

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -13,7 +13,7 @@ export const CardsContainer = () => {
     const productosQuery = useQuery({ 
         queryKey: ['productos'],
         queryFn: async() => {
-          const rs = await conn.get(`products/${'futbol'}?from=0&to=2`);
+          const rs = await conn.get(`products/${'futbol'}?from=0&to=3`);
           return rs.data;
         }
       })
