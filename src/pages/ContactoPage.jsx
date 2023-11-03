bc428c5f868ee70d224876c15fffe9d9f96b8aa0
Sebastian Morales <davesebastian99@gmail.com>
2023-11-03 13:14:48 -0500

    cambio de wp

--- src/pages/ContactoPage.jsx
+++ src/pages/ContactoPage.jsx
@@ -15,7 +15,7 @@ export const ContactoPage = () => {
     <div className="col-lg-6 mx-auto">
       <p className="lead mb-4">Somos la tienda deportiva por excelencia de la ciudad de Cali, hacemos envios a todo el país, animate y disfruta de nuestros productos que perduraran por generaciones</p>
       <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <Link type="button" className="btn btn-primary btn-lg px-4 gap-3"  to="https://chat.whatsapp.com/KmGiTQdOTGxGjFsqz135ve" >Enviar Mensaje</Link>
+        <Link type="button" className="btn btn-primary btn-lg px-4 gap-3"  to="https://chat.whatsapp.com/EroLwygUAM0KuxIrSCo0Ik" >Enviar Mensaje</Link>
         <Link type="button" className="btn btn-outline-secondary btn-lg px-4" to="/">Volver a Inicio</Link>
       </div>
     </div>
