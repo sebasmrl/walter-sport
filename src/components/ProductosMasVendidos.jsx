7a1f48e39101ba4b7624a572dfe6bb0d63cd7c4e
Sebastian Morales <davesebastian99@gmail.com>
2023-11-03 11:34:26 -0500

    adicion text

--- src/components/ProductosMasVendidos.jsx
+++ src/components/ProductosMasVendidos.jsx
@@ -5,7 +5,8 @@ export const ProductosMasVendidos = () => {
         <>
             <div className="container my-5">
                 <h3 className="display-5 text-center ">Productos mas vendidos.</h3>
-                <p className=" lead text-center  text-primary  mb-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim a ut ab iure, recusandae sint consectetur? Rerum officia fugiat soluta! Aliquid unde magnam in ducimus a sit ad tempore ratione?  </p>
+                <p className=" lead text-center  text-primary  mb-3">Descubre los productos favoritos de nuestros clientes, que te permiten disfrutar al maximo sus actividades deportivas.
+¿Por qué conformarte con menos cuando puedes elegir lo mejor?   </p>
             </div>
 
             <CardsContainer />
